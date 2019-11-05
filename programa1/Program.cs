eff08bb088018c3ae1b587a33e1d6e6504fd2474
andres9906 <stivenaereo@gmail.com>
2019-11-05 02:00:25 -0500

    Initial commit

---
+++ programa1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace scripts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string pattern ="[A-Z]:\\((\w|\W)[^\\]+(\S((\w|\W)[^\\])+)*\\)*"
+            int i = 0;
+            while(i == 0){
+                string pattern = @"(-)?\d.\d";
+                Regex rgx = new Regex(pattern);
+
+                Console.Write("Ingresar dirección: \n");
+                string input = Console.ReadLine();
+                Console.WriteLine(rgx.IsMatch(input));
+                Console.WriteLine("Desea seguir verificando direcciones? 0: Sí    1: No");
+                
+                i = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            //string input;
+            //Console.Write("Ingresar número decimal");
+            //input = Console.ReadLine();
+            //Console.WriteLine(input);
+        }
+    }
+}
---
+++ programa2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace programa2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while(i == 0){
+                Console.Write("Ingresar número a verificar: \n");
+                string input = Console.ReadLine();
+                Console.Write("Ingresar número decimales a verificar: \n");
+                string decimales = Console.ReadLine();
+
+                string pattern = @"(-)?\d.\d";
+                Regex rgx = new Regex(pattern);
+                
+                Console.WriteLine(rgx.IsMatch(input));
+                Console.WriteLine("Desea seguir verificando direcciones? 0: Sí    1: No");
+                i = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
