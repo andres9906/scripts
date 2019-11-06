abcd43755c74d3891147014d9630a8ab6f5d8212
andres9906 <stivenaereo@gmail.com>
2019-11-06 00:42:01 -0500

    new

---
+++ programa1_2/Decimales.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sww = 0;
+            while(sww == 0){
+                String dec;
+                Console.WriteLine("Ingrese el numero de decimales");
+                dec = Console.ReadLine();
+                Console.WriteLine("Ingrese el numero decimal");
+                String num = Console.ReadLine();
+
+                // using the method
+                String aux = "";
+                for (int i = 0; i < System.Convert.ToInt32(dec); i++)
+                {
+                    aux = "0" + aux;
+                }
+                String erDecimales = @"(-)?\d+.(\d{"+dec+"}(?<!" + aux + "))";
+                Match match = Regex.Match(num,erDecimales);
+                if (match.Success)
+                {   
+                    if(match.Value.Equals(num)){
+                        Console.WriteLine("Valido");
+                    }else{
+                        Console.WriteLine("No valido");
+                    }
+                }
+                else { 
+                    Console.WriteLine("No valido"); 
+                }
+
+                Console.WriteLine("Desea seguir verificando número decimales? 0: Sí    1: No");
+                sww = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
---
+++ programa2_2/Directorio.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace programa2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sww = 0;
+            while(sww == 0){        
+                String dir;
+                Console.WriteLine("Ingrese la direccion dos");
+                dir = Console.ReadLine();
+                Char aux = '"';
+                  String erDos = @"[A-Z]:\\((\w|\W[^/:*?<>"+Char.ToString(aux)+@"])+(\S(\w|\W[^/:*?<>"+Char.ToString(aux)+@"])+)*\\)*(\w|\W[^/:*?<>"+Char.ToString(aux)+@"\\])+(\S(\w|\W[^\\/:*?<>"+Char.ToString(aux)+@"])+)*(.((\w|\W[^/:*?<>"+Char.ToString(aux)+@"\\]))+)?";
+            
+                Match match = Regex.Match(dir,erDos);
+                if (match.Success)
+                {
+                if(match.Value.Equals(dir)){
+
+                    Console.WriteLine("Valido");
+                }else{
+                    Console.WriteLine("No valido");
+                }
+                }
+                else { Console.WriteLine("No valido");
+                }
+                Console.WriteLine("Desea seguir verificando número decimales? 0: Sí    1: No");
+                sww = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
